8d260ee24227cb1ea1a4ae8db5d014761158c9cf
Puripant <puripant@gmail.com>
2019-07-22 01:36:03 +0700

    Add dropdown to choose country

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const height = 600;
 const spacing = 120;
 const numPerRow = 7;
 const padding = 5;
+const max_drawing = 35;
+
+const $countries = $("#countries");
 
 let svg = d3.select("svg")
   .attr("width", width)
@@ -10,31 +13,49 @@ let svg = d3.select("svg")
 let g = svg.append("g");
     
 d3.json("all-penises.json", function(err, drawings) {
-  console.log(d3.nest().key(d => d.countrycode).rollup(v => v.length).entries(drawings));
+  let drawings_by_country = d3.nest().key(d => d.countrycode).object(drawings);
+  let countries = Object.keys(drawings_by_country);
 
-  let line =  d3.line()
-    .x(d => d.x)
-    .y(d => d.y)
-    .curve(d3.curveBasis);
-  
-  let groups = g.selectAll("g.drawing").data(drawings.filter(d => d.countrycode === "TH"));
-  let groupsE = groups.enter().append("g")
-    .classed("drawing", true)
-    .attr("transform", function(d, i) {
-      let x = (i % numPerRow) * spacing + padding;
-      let y = Math.floor(i/numPerRow) * spacing + padding;
-      return "translate(" + [x,y] + ") scale(0.4)";
-    })
-    .on("click", d => { console.log(d) })
-  let pathsE = groupsE.selectAll("path.stroke")
-    .data(d => d.drawing.map(function(s) {
-        let points = []
-        s[0].forEach((x,i) => { points.push({ x: x, y: s[1][i] }) })
-        return points;
-      })
-    )
-    .enter().append("path")
-    .classed("stroke", true);
-  
-  pathsE.attr("d", line);
+  countries.forEach(d => {
+    $countries.append($("<option>", {
+      value: d,
+      text: d
+    }));
+  });
+  $countries.dropdown({
+    onChange: (value, text, $selectedItem) => {
+      let country = $countries.dropdown('get value')[0];
+      $('#drawing-num').text(drawings_by_country[country].length);
+
+      svg.selectAll("g").remove();
+      draw(svg.append("g"), drawings_by_country[country]);
+    }
+  });
 });
+
+function draw(g, drawings) {
+  g.selectAll("g.drawing")
+    .data(drawings.slice(0, max_drawing))
+    .enter().append("g")
+      .classed("drawing", true)
+      .attr("transform", function(d, i) {
+        let x = (i % numPerRow) * spacing + padding;
+        let y = Math.floor(i/numPerRow) * spacing + padding;
+        return "translate(" + [x,y] + ") scale(0.4)";
+      })
+      // .on("click", d => { console.log(d) })
+      .selectAll("path.stroke")
+      .data(d => d.drawing.map(s => {
+          let points = []
+          s[0].forEach((x,i) => { points.push({ x: x, y: s[1][i] }) })
+          return points;
+        })
+      )
+      .enter().append("path")
+        .classed("stroke", true)
+        .attr("d", d3.line()
+          .x(d => d.x)
+          .y(d => d.y)
+          .curve(d3.curveBasis)
+        );
+}
